124d30657234c4e343e90d52c435f8de6e162233
Adrián Anta Gil <adranta@MacBook-Air-de-Anta.local>
2023-03-26 17:20:04 +0200

    Add fundido a negro SSE2

---
+++ AP2-Fundido-negro-SSE2/main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <emmintrin.h>
+#include <emmintrin.h>
+
+int main(int argc, char **argv) {
+
+    __m128i ceros;
+    ceros = _mm_set1_epi8((unsigned char) 0);
+
+    IplImage* inicial = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+    IplImage* transicion = cvCreateImage(cvSize(inicial->width, inicial->height), inicial->depth, inicial->nChannels);
+
+    int fila, cc,pasos;
+    for ( pasos = 0; pasos < 256; pasos++) {
+        for (fila = 0; fila < transicion->height; fila++) {
+
+            __m128i *pinicial = (__m128i *) (inicial->imageData + fila * inicial -> widthStep);
+            __m128i *ptransicion = (__m128i *) (transicion->imageData + fila * transicion->widthStep);
+
+            for (cc = 0; cc < transicion->widthStep; cc = cc + 16) {
+
+                *ptransicion = _mm_subs_epu8(*ptransicion, ceros);
+                *ptransicion = _mm_max_epu8(*ptransicion, ceros);
+
+                ptransicion++;
+                pinicial++;
+            }
+        }
+
+        cvNamedWindow("Componente Original", CV_WINDOW_AUTOSIZE);
+        cvShowImage("Componente Original", inicial);
+
+        cvNamedWindow("Componente Copiado", CV_WINDOW_AUTOSIZE);
+        cvShowImage("Componente Copiado", transicion);
+        cvWaitKey(20);
+    }
+    cvReleaseImage(&transicion);
+    cvReleaseImage(&inicial);
+
+
+    cvDestroyWindow(argv[1]);
+    cvDestroyWindow("Componente Copiado");
+
+    return EXIT_SUCCESS;
+}
