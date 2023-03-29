c5733638bcef6aa98a44ad6e3353edccd4ddd22d
Adrián Anta <103943048+antaga04@users.noreply.github.com>
2023-03-29 18:24:54 +0200

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -9,8 +9,8 @@
 int main(int argc, char** argv) {
 
     if (argc != 2) {
-    printf("Error: Usage %s image_file_name\n", argv[0]);
-    return EXIT_FAILURE;
+        printf("Error: Usage %s image_file_name\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     IplImage* Img = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
@@ -23,53 +23,69 @@ int main(int argc, char** argv) {
 
     // a visualization window is created with title: image file name
     cvNamedWindow(argv[1], CV_WINDOW_AUTOSIZE);
+    
     // Img is shown in 'image' window
     cvShowImage(argv[1], Img);
     cvWaitKey(0);
 
-
     // Copiar imagen
-    IplImage* ImgEj1 = cvCreateImage(cvSize(Img->width, Img->height), IPL_DEPTH_8U, 4);
-  
+    IplImage* ImgEj = cvCreateImage(cvSize(Img->width, Img->height), IPL_DEPTH_8U, 4);
+
+    int fila, cc;
+    for (fila = 0; fila < Img->height; fila++) {
+
+        __m128i *pImg = (__m128i *) (Img->imageData + fila * Img-> widthStep);
+        __m128i *pImgEj = (__m128i *) (ImgEj->imageData + fila * ImgEj->widthStep);
+
+        for (cc = 0; cc < Img->widthStep; cc = cc + 16) {
+
+            *pImgEj++ = *pImg;
+            pImg++;
+        }
+    }
+
     // Componentes RG (segundo cuadrante)
     __m128i rg = _mm_set1_epi32(0xFFFF00);
     // Componentes RG (tercer cuadrante)
     __m128i gb = _mm_set1_epi32(0x00FFFF);
     // Componentes RG (cuarto cuadrante)
     __m128i rb = _mm_set1_epi32(0xFF00FF);
-  
-    int fila, cc;
+    // white
+    __m128i w = _mm_set1_epi32(0x00);
+
     for (fila = 0; fila < Img->height; fila++) {
-        
-        __m128i *pImgEj4 = (__m128i *) (ImgEj4->imageData + fila * ImgEj4->widthStep);
-        
+        __m128i *pImg = (__m128i *) (Img->imageData + fila * Img-> widthStep);
+        __m128i *pImgEj = (__m128i *) (ImgEj->imageData + fila * ImgEj->widthStep);
+
         for (cc = 0; cc < Img->widthStep; cc = cc + 16) {
-          if(cc < 800 && fila < 800){
-            // Primer cuadrante RGB
-            *pImgEj4++ = *pImg;
-          }else if(cc >800 && fila < 800){
-            __m128i PuertaRG = _mm_and_si128(rg, *pImgEj4);
-            _mm_storeu_si128(pImgEj4, PuertaRG);
-          }
-          else if(cc > 800 && fila > 800 && fila < 1300){
-            __m128i PuertaRG = _mm_and_si128(rg, *pImgEj4);
-            _mm_storeu_si128(pImgEj4, PuertaRG);
-          }
-          else if(cc > 800 && fila > 1300){
-            __m128i PuertaRG = _mm_and_si128(rg, *pImgEj4);
-            _mm_storeu_si128(pImgEj4, PuertaRG);
-          }
-            pImg++;
+            if (cc < 1601 && fila > 200) {
+                __m128i PuertaW = _mm_and_si128(w, *pImgEj);
+                _mm_storeu_si128(pImgEj, PuertaW);
+            }
+            if (cc > 1600 && fila < 200) {
+                __m128i PuertaRG = _mm_and_si128(rg, *pImgEj);
+                _mm_storeu_si128(pImgEj, PuertaRG);
+            }
+
+            if (cc > 1600 && fila > 200) {
+                __m128i PuertaGB = _mm_and_si128(gb, *pImgEj);
+                _mm_storeu_si128(pImgEj, PuertaGB);
+            }
+            if (cc > 1600 && fila > 400) {
+                __m128i PuertaRB = _mm_and_si128(rb, *pImgEj);
+                _mm_storeu_si128(pImgEj, PuertaRB);
+            }
+            pImgEj++;
         }
     }
-    
+
     cvNamedWindow("Ejericio 1", CV_WINDOW_AUTOSIZE);
     cvShowImage("Ejericio 1", ImgEj);
     cvWaitKey(0);
 
     // memory release for images before exiting the application
     cvReleaseImage(&ImgEj);
-    
+
     // Self-explanatory
     cvDestroyWindow("Ejericio 1");
 
