696f4ef76793cdbbe017f0513fff791079277a87
Adrián Anta <103943048+antaga04@users.noreply.github.com>
2023-03-30 17:47:28 +0200

    Create ferrari-prueba.c

---
+++ ferrari-prueba.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <emmintrin.h>
+#include <emmintrin.h>
+
+int main(int argc, char **argv) {
+
+    __m128i ceros;
+    ceros = _mm_set1_epi8((unsigned char) 0);
+
+    __m128i unos;
+    unos = _mm_set1_epi8((unsigned char) 1);
+
+    IplImage* inicial = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+    IplImage* transicion = cvCreateImage(cvSize(inicial->width, inicial->height), inicial->depth, inicial->nChannels);
+
+    int fila, cc, pasos;
+    for (pasos = 0; pasos < 256; pasos++) {
+        for (fila = 0; fila < transicion->height; fila++) {
+
+            __m128i *pinicial = (__m128i *) (inicial->imageData + fila * inicial -> widthStep);
+//            __m128i *ptransicion = (__m128i *) (transicion->imageData + fila * transicion->widthStep);
+
+            for (cc = 0; cc < transicion->widthStep; cc = cc + 16) {
+
+                __m128i A = _mm_loadu_si128(pinicial);
+                __m128i B = _mm_adds_epu8(A, unos);
+                _mm_storeu_si128(pinicial,B);
+                
+                pinicial++;
+            }
+        }
+
+        cvNamedWindow("Fade a Blanco", CV_WINDOW_AUTOSIZE);
+        cvShowImage("Fade a Blanco", inicial);
+        cvWaitKey(20);
+    }
+    cvReleaseImage(&transicion);
+    cvReleaseImage(&inicial);
+
+
+    cvDestroyWindow(argv[1]);
+    cvDestroyWindow("Fade a Blanco");
+
+    return EXIT_SUCCESS;
+}
