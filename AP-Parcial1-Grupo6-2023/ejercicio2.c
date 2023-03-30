7f0df4d5c92fb515ca2f33407dca77ffa5009ba3
Adrián Anta <103943048+antaga04@users.noreply.github.com>
2023-03-30 20:26:43 +0200

    Update ejercicio2.c

--- AP-Parcial1-Grupo6-2023/ejercicio2.c
+++ AP-Parcial1-Grupo6-2023/ejercicio2.c
@@ -1,50 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <emmintrin.h>
 
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
-    IplImage* inicial = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
-
-    IplImage* transicion = cvCreateImage(cvSize(inicial->width, inicial->height), IPL_DEPTH_32F, 3);
-
-    float *pinicial, *ptransicion;
-    int fila, cc, pasos;
-
-    for (fila = 0; fila < inicial->height; fila++) {
-        pinicial = (float *) (inicial->imageData + fila * inicial->widthStep);
-        ptransicion = (float *) (transicion->imageData + fila * transicion->widthStep);
-
-        for (cc = 0; cc < inicial->width * 3; cc += 3) {
-            ptransicion[cc] = pinicial[cc] / 255.0;
-            ptransicion[cc + 1] = pinicial[cc + 1] / 255.0;
-            ptransicion[cc + 2] = pinicial[cc + 2] / 255.0;
-        }
+    if (argc != 2) {
+        printf("Usage: %s image_file_name\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
-    for (pasos = 0; pasos < 256; pasos++) {
-        for (fila = 0; fila < transicion->height; fila++) {
-            ptransicion = (float *) (transicion->imageData + fila * transicion->widthStep);
+    IplImage* imagenInicial = cvLoadImage(argv[1], CV_LOAD_IMAGE_UNCHANGED);
+    if (!imagenInicial) {
+        printf("Error: %s fichero no leido", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-            for (cc = 0; cc < transicion->width * 3; cc += 3) {
-                ptransicion[cc] += 1.0 / 255.0;
-                ptransicion[cc + 1] += 1.0 / 255.0;
-                ptransicion[cc + 2] += 1.0 / 255.0;
+    IplImage* imagenInicialFloat = cvCreateImage(cvSize(imagenInicial->width, imagenInicial ->height), IPL_DEPTH_32F, imagenInicial->nChannels);
+    cvConvertScale(imagenInicial, imagenInicialFloat, 1.0 / 255.0, 0);
+    cvShowImage(argv[1], imagenInicialFloat);
+    cvWaitKey(0);
+
+
+    IplImage* imagenFinal = cvLoadImage(argv[2], CV_LOAD_IMAGE_UNCHANGED);
+    if (!imagenFinal) {
+        printf("Error: %s fichero no leida", argv[2]);
+        return EXIT_FAILURE;
+    }
+
+    IplImage* imagenFinalFloat = cvCreateImage(cvSize(imagenFinal->width, imagenFinal ->height), IPL_DEPTH_32F, imagenFinal->nChannels);
+    cvConvertScale(imagenFinal, imagenFinalFloat, 1.0 / 255.0, 0);
+
+    IplImage* imagenTransicionFloat = cvCreateImage(cvSize(imagenInicialFloat->width, imagenInicialFloat ->height), IPL_DEPTH_32F, imagenInicialFloat->nChannels);
+    cvShowImage("imagenTransicionFloat", imagenTransicionFloat);
+    cvWaitKey(10);
+
+
+    int paso;
+
+    int iteraciones = 100;
+
+    int fil, cc;
+
+    for (paso = 0; paso <= iteraciones; paso++) {
+
+        float pasoFloat = ((float) paso) / (float) (iteraciones);
+        __m128 pasoSSE = _mm_set_ps1(pasoFloat);
+        __m128 pasoMenosUnoSSE = _mm_set_ps1(1.0 - pasoFloat);
+
+        for (fil = 0; fil < imagenInicialFloat->height; fil++) {
+
+            __m128 *pimagenInicialFloat = (__m128 *) (imagenInicialFloat->imageData + fil * imagenInicialFloat->widthStep);
+            __m128 *pimagenTransicionFloat = (__m128 *) (imagenTransicionFloat->imageData + fil * imagenTransicionFloat->widthStep);
+
+            for (cc = 0; cc < imagenInicialFloat->widthStep; cc = cc + 16) {
+
+                __m128 ccInicial, ccFinal, ccTransicion;
+
+                ccInicial = *pimagenInicialFloat++;
+                ccInicial = _mm_div_ps(ccInicial, pasoMenosUnoSSE);
+                *pimagenTransicionFloat++ = ccInicial;
             }
-        }
 
-        cvNamedWindow("Fade a Blanco", CV_WINDOW_AUTOSIZE);
-        cvShowImage("Fade a Blanco", transicion);
-        cvWaitKey(20);
+
+        }
+        cvWaitKey(30);
+        cvShowImage("imagenTransicionFloat", imagenTransicionFloat);
     }
 
-    cvReleaseImage(&transicion);
-    cvReleaseImage(&inicial);
 
-    cvDestroyWindow(argv[1]);
-    cvDestroyWindow("Fade a Blanco");
 
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
